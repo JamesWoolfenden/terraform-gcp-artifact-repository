25f510a924e8a4407c6f90d43954c7a6d5507256
James Woolfenden <james.woolfenden@gmail.com>
2023-11-16 14:24:15 +0000

    fix var names

--- example/examplea/provider.google.tf
+++ example/examplea/provider.google.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project     = "pike-gcp"
+  project = "pike-gcp"
   # credentials = "/Users/jwoolfenden/pike-gcp-super.json"
 }
 
 provider "google-beta" {
-  project     = "pike-gcp"
+  project = "pike-gcp"
   # credentials = "/Users/jwoolfenden/pike-gcp-super.json"
 }
