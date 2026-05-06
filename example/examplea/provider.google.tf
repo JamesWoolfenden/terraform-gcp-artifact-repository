4cb318fc1e950fdf3592e754ed35f7d9f6511e52
James Woolfenden <james.woolfenden@gmail.com>
2026-05-06 23:37:51 +0100

    fix: update pre-commit hook versions and provider project

--- example/examplea/provider.google.tf
+++ example/examplea/provider.google.tf
@@ -1,9 +1,9 @@
 provider "google" {
   project = "pike-477416"
-  #credentials = "/Users/jwoolfenden/pike-gcp-super.json"
+  #credentials = "/Users/jwoolfenden/pike-477416-super.json"
 }
 
 provider "google-beta" {
   project = "pike-477416"
-  #credentials = "/Users/jwoolfenden/pike-gcp-super.json"
+  #credentials = "/Users/jwoolfenden/pike-477416-super.json"
 }
